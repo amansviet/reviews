d179f81e01497c2ea08e9824bff27ffff333f627
Amandeep Bhatia <amanbhatia.canada@gmail.com>
2021-01-26 16:19:33 -0500

    Updates

---
+++ precache-manifest.68b39d43c023e4e35f0eee95a32f41a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29299a6fe7b67445956286c50dfa771",
+    "url": "/reviews/index.html"
+  },
+  {
+    "revision": "7e9d1eaf190d76d0339f",
+    "url": "/reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9d1eaf190d76d0339f",
+    "url": "/reviews/static/js/main.b5cbe94b.chunk.js"
+  },
+  {
+    "revision": "5496dc53684f16f6acfe",
+    "url": "/reviews/static/js/runtime-main.51a67177.js"
+  }
+]);
